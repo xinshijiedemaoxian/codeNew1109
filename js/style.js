8e398e1bfa3a64527c79886fb67ec523111db18a
xinshijiedemaoxian <xia_duanjian@163.com>
2016-11-09 21:34:58 +0800

    05

--- js/style.js
+++ js/style.js
@@ -1,9 +1,10 @@
 var qrcode = new QRCode(document.getElementById("qrcode"), {});
 function browserRedirect() {
     if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
+        hechen();
         $(".button").on("touchstart", function (e) {
             makeCode();
-            
+
         }).on("touchend ", function (e) {
             hechen();
             event.preventDefault()
