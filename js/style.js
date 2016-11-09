ffb8842925a7ce1b87d0a8ba70111f317d42e31f
xinshijiedemaoxian <xia_duanjian@163.com>
2016-11-09 22:15:59 +0800

    11

--- js/style.js
+++ js/style.js
@@ -3,18 +3,18 @@ function browserRedirect() {
     if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
         $(".button").on("touchstart", function (e) {
             makeCode();
-            event.preventDefault()
+
         });
         $(".button").on("touchend ", function (e) {
-            $(".button").style.color="red";
             hechen();
-            event.preventDefault();
+
         })
     } else {
         $(".button").on("mousedown", function (e) {
             makeCode()
         }).on("mouseup ", function (e) {
-            hechen()
+            hechen();
+
         })
     }
 }
